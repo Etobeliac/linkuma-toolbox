8e9f6d4675fd288bf33564d3a5fbc5840fce9ce2
Etobeliac <celestin.clbt@gmail.com>
2024-10-22 17:17:40 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,6 +17,7 @@ option = st.sidebar.selectbox(
 def load_module(module_name, file_path):
     if not os.path.isfile(file_path):
         st.error(f"Le fichier {file_path} est introuvable. Veuillez vérifier le chemin.")
+        st.write(f"Attempting to load from: {file_path}")  # Debug information
         return None
     spec = importlib.util.spec_from_file_location(module_name, file_path)
     module = importlib.util.module_from_spec(spec)
@@ -27,12 +28,23 @@ if option == 'Tutoriel':
     # Show tutorial information
     st.header("Tutoriel - Guide d'Utilisation")
     st.write("""
-    Loading
+    **Bienvenue dans l'application !**
+    
+    Cette application vous permet de réaliser différentes tâches via des scripts automatisés :
+    
+    - **Create Collections** : Crée des collections et catégories pour vos produits en utilisant ChatGPT.
+    
+    ### Comment Utiliser :
+    
+    - Sélectionnez "Create Collections" dans le menu latéral pour accéder à cette fonctionnalité.
+    - Remplissez les informations nécessaires et lancez le processus pour générer des collections.
+    
+    *Pour toute question supplémentaire, n'hésitez pas à contacter notre équipe de support.*
     """)
 
 elif option == 'Create Collections':
     # Path to the script for creating collections
-    file_path = os.path.join('scripts', 'creation-collections.py')
+    file_path = os.path.join(os.getcwd(), 'scripts', 'creation-collections.py')
     
     # Load and execute the module
     module = load_module('creation_collections', file_path)
@@ -40,4 +52,3 @@ elif option == 'Create Collections':
     # Call the main function of the module if successfully loaded
     if module:
         module.main()
-
