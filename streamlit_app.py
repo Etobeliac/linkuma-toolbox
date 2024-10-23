34ac3060061da0c6a1b7022e5bebec42865ee79e
Etobeliac <celestin.clbt@gmail.com>
2024-10-23 10:05:58 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,19 @@
+import streamlit as st
 import scripts.utils as utils
 
-# Appel à la fonction pour exécuter l'application
+# Menu principal pour choisir l'opération
+def main():
+    st.sidebar.title("Menu Principal")
+    option = st.sidebar.selectbox(
+        "Choisissez une option",
+        ("Accueil", "Créer et catégoriser des collections")
+    )
+    
+    if option == "Accueil":
+        st.title("Bienvenue sur l'application de gestion de collections")
+        st.write("Utilisez le menu à gauche pour commencer.")
+    elif option == "Créer et catégoriser des collections":
+        utils.run_create_and_categorize()
+
 if __name__ == "__main__":
-    utils.run_create_and_categorize()
+    main()
